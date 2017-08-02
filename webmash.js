95bac9b6f7148876f26f0915ff73df3552035ee4
mtayyab-s <tayyabsadiq@ymail.com>
2017-08-01 19:06:49 -0600

    updated the files

--- webmash.js
+++ webmash.js
@@ -1,23 +1,86 @@
-// Put your zillow.com API key here
+ //Put your zillow.com API key here
+/*
+    Student Name:Muhammad Tayyab, 
+*/
 
-var username = "????";
+var username = "mtayyab12";
 var request = new XMLHttpRequest();
+var address;
+var marker;
+var infowindow;
 
 
-//initMap() which initiates map to a location
-function initMap() {
-
-	//initialize map
+//initialize() which initiates map to a location
+function initialize() {
+  var latLng = new google.maps.LatLng(32.75, -97.13); 
+  var mapProp = {
+    center:latLng,
+    zoom:17,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+  var map=new google.maps.Map(document.getElementById("map"),mapProp);
+  infowindow = new google.maps.InfoWindow();
+   
+   //initialize map
 	
 	//Initialize a mouse click event on map which then calls reversegeocode function
- 
+  google.maps.event.addListener(map,'click',function(evt) {
+   
+   placeMarker(evt.latLng); 
+   reversegeocode();
+   
+   
+  });
+ //placeMarker() helps to set the marker on the map where it is clicked
+ function placeMarker(location) {
+    if (marker == undefined){
+        marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        animation: google.maps.Animation.BOUNCE
+      });
+     }
+    else{
+        marker.setPosition(location);
+        }
+    map.setCenter(location);
+             
+        }
 }
 
 
 
-// Reserse Geocoding 
-function reversegeocode() {
-
+// Reserse Geocoding i-e  getting address from the given marker's coordinates 
+function reversegeocode() {   
+	var coordinates  = marker.getPosition();
+	var lats = marker.getPosition().lat();
+	var lngs = marker.getPosition().lng();
+	var geocoder = new google.maps.Geocoder;
+	
+  //console.log(lats + " " + lngs);
+	geocoder.geocode({'latLng': coordinates}, function(results, status)
+    {
+        if (status == google.maps.GeocoderStatus.OK)
+        {
+            if (results[0])
+            {
+               
+				address= results[0].formatted_address;
+			}
+            else
+            {
+                alert("No results found");
+            }
+        }
+        else
+        {
+            alert("Geocoder failed due to: " + status);
+        }
+		console.log(address);
+    sendRequest(lats,lngs);
+    });
+	
+	
   //get the latitude and longitude from the mouse click and get the address.
   //call geoname api asynchronously with latitude and longitude 
   
@@ -26,18 +89,40 @@ function reversegeocode() {
 
 
 function displayResult () {
-    if (request.readyState == 4) {
+   
+
+   if (request.readyState == 4) {
+	   console.log("in the displayResult fuinction");
         var xml = request.responseXML.documentElement;
-        var temperature = xml.getElementsByTagName("temperature");
-	document.getElementById("output").innerHTML = value;
+        var temperature = xml.getElementsByTagName("temperature")[0].childNodes[0].nodeValue;
+		var windspeed = xml.getElementsByTagName("windSpeed")[0].childNodes[0].nodeValue;
+		var clouds = xml.getElementsByTagName("clouds")[0].childNodes[0].nodeValue;
+	    console.log("temp equals"+temperature);
+	    var div = document.getElementById("output");
+	    div.innerHTML = div.innerHTML+"&#13"+ 
+	                   "address: " +address+"&#13" + 
+	                   "temperature:" +temperature+"&#13"+
+	                   "wind speed: "+windspeed+"&#13"+
+					   "clouds: "+clouds+"&#13";
+	
+	    console.log(address);
+	    infowindow.setContent("address: "+address+ '</br>'+ "temperature: "+temperature+ '</br>'+ "windspeed: "+windspeed+'</br>'+ "clouds: "+clouds);
+	    infowindow.open(map, marker);
     }
 }
 
-function sendRequest () {
+function sendRequest (lats,lngs) {
     request.onreadystatechange = displayResult;
-    var lat = "";
-    var lng = "";
-    request.open("GET"," http://api.geonames.org/findNearByWeatherXML?lat="+lat+"&lng="+lng+"&username="+username);
-    request.withCredentials = "true";
+    var lat = lats;
+    var lng = lngs;
+	var uri = encodeURI("http://api.geonames.org/findNearByWeatherXML?lat="+lat+"&lng="+lng+"&username="+username);
+    request.open("GET",uri,true);
+    //request.withCredentials = "true";
     request.send(null);
+	
+}
+
+function eraseText(){
+	document.getElementById("output").innerHTML = "";
+	
 }
